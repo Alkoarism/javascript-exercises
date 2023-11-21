91510767d2ed047c507ae5c2f5a3cfd461e775a9
alkoarism <69710741+Alkoarism@users.noreply.github.com>
2023-11-20 23:30:35 -0300

    EXERCISE: Solved from 1 to 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversed = "";
+    const textSize = text.length;
+    for (let i = textSize - 1; i >= 0; i--){
+        reversed += text[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...undesired) {
+    const undesiredItems = Array.from(undesired); //Removes repeated items in undesired
+    let result = [];
 
+    const pushElementConditionaly = (element) => {
+        if(!undesiredItems.includes(element)) {result.push(element);}
+    }
+
+    array.map(pushElementConditionaly);
+    return result;
 };
 
 // Do not edit below this line
